3278628aa60e42e292d9d9ff60aef01e6395f20e
yogeshpednekar <yogeshpednekaryogi@gmail.com>
2022-07-18 22:52:09 +0530

    1 file modified

--- index.ts
+++ index.ts
@@ -4,3 +4,9 @@ import './style.css';
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
+let word = 'Welcome to Type script';
+console.log(word);
+
+let str1 = (String = 'Hello');
+console.log(str1);
